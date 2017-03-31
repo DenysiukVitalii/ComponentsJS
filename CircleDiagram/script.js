d504cf9c0e6306c9c7ef207b98eb083f69b6267b
vitalii_denysiuk <dendoc97@gmail.com>
2017-03-31 21:20:39 +0300

    clean code

--- CircleDiagram/script.js
+++ CircleDiagram/script.js
@@ -5,7 +5,11 @@ var drawCircle = function(selector){
         var valueCircle = parseFloat(el.innerHTML);
         valueCircle = Math.round(valueCircle*408/100);
         var animationCircle = setInterval(function() {
-          el.innerHTML = '<svg width="160" height="160"><circle transform="rotate(-90)" r="65" cx="-80" cy="80" /><circle transform="rotate(-90)" style="stroke-dasharray:'+ counterCircle +'px 408px;" r="65" cx="-80" cy="80" /></svg><span class="percents"></span>';
+          el.innerHTML = '<svg width="160" height="160">' +
+            '<circle transform="rotate(-90)" r="65" cx="-80" cy="80" />' +
+            '<circle transform="rotate(-90)" style="stroke-dasharray:'+ counterCircle +
+            'px 408px;" r="65" cx="-80" cy="80" /></svg>'+
+            '<span class="percents"></span>';
           var percent = el.children[1];
           percent.innerHTML = Math.round(counterCircle*100/408) + '%';
           if (counterCircle == valueCircle) clearInterval(animationCircle);
